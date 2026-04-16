53671ba84b551c24b76a41f468311391539d33e0
JayantOlhyan <jayantolhyan@gmail.com>
2026-04-16 22:36:02 +0530

    feat: automate sitemap generation using vite-plugin-sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,58 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      hostname: 'https://msit-website.netlify.app',
+      dynamicRoutes: [
+        '/',
+        '/search',
+        '/faculty',
+        '/admin',
+        '/privacy',
+        '/terms',
+        '/500',
+        '/vision-mission',
+        '/history',
+        '/about',
+        '/administration',
+        '/govern',
+        '/fromdesk',
+        '/facilities',
+        '/smes',
+        '/contact',
+        '/cse',
+        '/it',
+        '/ece',
+        '/eee',
+        '/applied-sciences',
+        '/academic-calendar',
+        '/timetable',
+        '/research',
+        '/brochure',
+        '/online-fee',
+        '/scholarships',
+        '/mandatory-disclosures',
+        '/society',
+        '/events',
+        '/antiragging',
+        '/posh',
+        '/disaster',
+        '/discipline',
+        '/placements',
+        '/internship-cell',
+        '/student-portal',
+        '/alumni-network',
+        '/society-ieee'
+      ]
+    })
+  ],
   base: '/',
 })
+
